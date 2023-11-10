f05b95426d1974f20a2a90fedfef297fc165c2f8
petmila <mila-petrenko@inbox.ru>
2023-11-11 01:32:16 +0300

    fix font

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,17 @@
-function activeMenu(){
+// function activeMenu(){
     var i, menu_items;
 
     menu_items = document.getElementsByClassName("bookmark_menu__bookmark");
     for (i = 0; i < menu_items.length; i++) {
-        if (document.location.href === menu_items[i].attributes.href.value){
+        console.log(menu_items[i].attributes);
+        if (document.location.href === menu_items[i].attributes.href.baseURI){
             menu_items[i].style.display += " active";
+            console.log("success");
         }
     }
-    console.log(document.location.href);
-}
+    // console.log(document.location.href);
+// }
 
-window.addEventListener("load", function(event) {
-    activeMenu()
-}, false)
+// window.addEventListener("load", function(event) {
+//     activeMenu()
+// }, false)
